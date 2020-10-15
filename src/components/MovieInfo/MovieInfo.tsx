3840dd4f4e24d6c54b0a772ede0880d9b52a432a
Ingrid Gomo <54813201+IngridGomo@users.noreply.github.com>
2020-10-15 15:16:55 +0200

    made layout with react

--- src/components/MovieInfo/MovieInfo.tsx
+++ src/components/MovieInfo/MovieInfo.tsx
@@ -15,12 +15,12 @@ export default function MovieBox(props:Props) {
     
     return (
         <div id="movieInfo">
-            <h3>{props.movieTitle}</h3>
+            <h3 id="movieInfoTitle">{props.movieTitle}</h3>
             <hr/>
-            <p>Duration: {props.duration}</p>
-            <p>Genre: {props.genre}</p>
-            <p>Directors: {props.directors}</p>
-            <p>Main actors: {props.actor1}, {props.actor2}, {props.actor3}</p>
+            <p className="movieInfoP">Duration: {props.duration}</p>
+            <p className="movieInfoP">Genre: {props.genre}</p>
+            <p className="movieInfoP">Directors: {props.directors}</p>
+            <p className="movieInfoP">Main actors: {props.actor1}, {props.actor2}, {props.actor3}</p>
         </div>
     )
 }
