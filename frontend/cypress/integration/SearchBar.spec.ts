4f813f6618995368073bd3321971a0888d51d211
Simen Sverdrup-Thygeson <simenthygeson@gmail.com>
2020-11-04 11:39:12 +0100

    #8 #12 fix cypress + snapshot test

--- frontend/cypress/integration/SearchBar.spec.ts
+++ frontend/cypress/integration/SearchBar.spec.ts
@@ -2,10 +2,9 @@ describe('Type in SearchBar', function() {
     it('Accepts input', function() {
         cy.visit('/');
 
-        cy.get('searchInput')
+        cy.get('.searchInput')
             .type('Game Night')
             .should('have.value', 'Game Night');
-
     })
 });
 
